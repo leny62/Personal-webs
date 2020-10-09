7218c524667aa16857f991f4f54b0a88d2d75f8e
leny Pascal IHIRWE <lihirwe6@gmail.com>
2020-10-08 23:53:54 -0700

    ft(pages) :Worked on responsiveness of About, sign in, sign up and contact page

---
+++ js/blogs.js
@@ -0,0 +1,100 @@
+// let db = firebase.firestore();
+// let storage = firebase.storage();
+// let auth = firebase.auth();
+
+function uploadImage(event){
+  if(event.files[0] != null){
+    blogImage = event.files[0];
+  }
+}
+
+
+let blogImage;
+let blogs = [];
+function uploadImage(event){
+    if(event.target.files[0] != null){
+        blogImage = event.target.files[0];
+    }
+}
+
+function createBlog(){
+    let today = new Date();
+    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+   
+    let blogTitle = document.getElementById('title').value;
+    let blogContnent = document.getElementById('body').value;
+    let blogOwner = "John Doe";
+    let blogId = generateBlogId();
+    // let imageURL = ''
+    
+    storage.ref(`blogs/${blogId}/blogImage.jpg`).put(blogImage).then(()=>{
+        db.collection('Blogs').doc(`${blogId}`).set({
+            title: blogTitle,
+            blogBody: blogContnent,
+            owner: blogOwner,
+            imageURL: `blogs/${blogId}/blogImage.jpg`,
+            date: date
+    
+        }).then(()=>{
+              
+        }).catch((collectionError)=>{
+            alert(collectionError);
+        })
+    
+    })
+    .catch((storageError) =>{
+        alert(storageError);
+    })
+    
+    
+}
+
+function fetchData(){
+    db.collection("Blogs").get().then((querySnapshot) => {
+      querySnapshot.forEach((blog)=> {
+        
+        // doc.data() is never undefined for query doc snapshots
+        // console.log(blog.id, " => ", blog.data());
+        storage.ref(blog.data().imageURL).getDownloadURL().then((blogImageUrl)=>{
+           blogs.push({
+               title: blog.data().title,
+               blogContent: blog.data().blogBody,
+               date: blog.data().date,
+               owner: blog.data().owner,
+               imageSrc: blogImageUrl
+
+           })
+        }).catch((downUrlError)=>{
+            console.log(downUrlError)
+        })
+
+
+    });
+}).catch((fetchingError)=>{
+    console.log(fetchingError)
+})
+
+}  
+
+function dipslayBlogPosts(){
+   let postTitle = document.getElementById('headingWrapper');
+   let postImage = document.getElementById('imageWrapper');
+   let postBody = document.getElementById('contentWrapper');
+   let postDate = document.getElementById('dateWrapper');
+   let postAuthor = document.getElementById('ownerWrapper');
+       let size = blogs.length;
+
+       postTitle.innerHTML = blogs[size-1].title;
+       postImage.src = blogs[size-1].imageSrc;
+       postBody.innerHTML = blogs[size-1].blogContent;
+       postDate.innerHTML = blogs[size-1].date;
+       postAuthor.innerHTML = blogs[size-1].owner;
+   
+}
+
+
+fetchData();
+
+setTimeout(()=>{
+    dipslayBlogPosts()
+},5000)
---
+++ js/contact.js
@@ -0,0 +1,36 @@
+let bt = document.getElementsByClassName("btn");
+bt.onclick = validate;
+function validate(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    var error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    var text;
+    if(name.length ==""){
+      error_message.style.visibility = 'visible';
+      text = "Please Enter valid Name";
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+      text = "Please Enter valid Email";
+      error_message.style.visibility = 'visible';
+        // check.innerHTML = 'email must be valid';
+      error_message.innerHTML = text;
+      return false;
+    }
+    if(message.length <= 140){
+      text = "Please Enter More Than 140 Characters";
+      error_message.innerHTML = text;
+      return false;
+    } else{
+      db.collection("message").add("{}")
+;    }
+      alert("Form Submitted Successfully!");
+      return true;
+    }
+    
+  }
