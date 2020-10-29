af12d79f790d9f55691d418316d1498ee76f1281
leny62 <lihirwe6@gmail.com>
2020-10-29 17:36:04 +0200

    ft(blog page) : added a comment section and name display on the blog pages

--- js/blogcomment.js
+++ js/blogcomment.js
@@ -1,8 +1,11 @@
-
 const post = (event) => {
     event.preventDefault();
     let com = document.getElementById('comment').value;
+    let name = document.getElementById('name').value;
+    let p = document.createElement('p');
     let div = document.createElement('div');
+    p.style.color = "blue";
+    p.innerHTML = "by: " +name;
     div.style.width = "200px";
     div.style.height = "50px";
     div.style.padding = "4px";
@@ -10,8 +13,10 @@ const post = (event) => {
     div.style.backgroundColor = "whitesmoke";
     div.style.color = 'black';
     div.innerHTML = com;
+    document.getElementById("allcomments").appendChild(p);
     document.getElementById("allcomments").appendChild(div);
     document.getElementById("form-input").reset();
+
 };
 
 
